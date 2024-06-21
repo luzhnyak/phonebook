41118284b83b9ddda98a45a1d2394f93f2281087
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-06-21 15:13:12 +0300

    add https to api

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -4,7 +4,7 @@ import { RootState } from "./store";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://luzhnyak-dev.pp.ua:84",
+    baseUrl: "https://luzhnyak-dev.pp.ua",
     prepareHeaders: (headers, { getState }) => {
       // By default, if we have a token in the store, let's use that for authenticated requests
       const token = (getState() as RootState).auth.token;
